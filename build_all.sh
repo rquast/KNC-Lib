cf5c39dfc44ea99949d9c2b6c9c3d504c76294b0
coast <robertpancoast77@gmail.com>
2016-10-02 16:34:19 -0400

    TODO

--- build_all.sh
+++ build_all.sh
@@ -3,6 +3,13 @@
 KNC_LIB=$PWD
 echo $KNC_LIB
 
+# TODO:
+# add additional KNC-LIBS
+# boost_1_60_0
+# microhttpd-0.9.28
+# gmp-6.1.0
+# json-rpc-cpp-0.5.0 < -requires microhttpd
+
 #cd $KNC_LIB/gmp-6.1.0/ && \
 #sh $KNC_LIB/gmp-6.1.0/build.bot.log.sh
 #cd $KNC_LIB/boost_1_60_0/ && \
