1983da4b2d3853536c76975175b8197614e5cef3
coast <robertpancoast77@gmail.com>
2016-10-02 15:40:30 -0400

    freetype2

---
+++ freetype2/build.bot.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Build Systems #
+SRC=$PWD
+cd $SRC && git rev-parse HEAD
+
+# Incremental Build #
+INCREMENTDISABLE=TRUE
+
+# Dual Build #
+#XEONX64BUILD=TRUE
+XEONPHIBUILD=TRUE
+
+
+if test ${XEONPHIBUILD+defined}; then
+
+# XEON PHI Build #
+echo "XEON PHI Build"
+BUILDTREE=$SRC/build-mic
+MPSSDIR=/home/mpss-3.7
+
+# Clean Build Tree #
+if test ${INCREMENTDISABLE+defined}; then
+echo "Incremental Build disabled"
+rm -rf $BUILDTREE
+else
+echo "Incremental Build enabled"
+fi
+mkdir -p $BUILDTREE
+
+# Build k1om #
+
+cd $BUILDTREE \
+&& CC=icc CXX=icpc \
+CXXFLAGS="-g -O2 -fPIC -mmic" \
+CFLAGS="-g -O2 -fPIC -mmic" \
+cmake3 \
+-DCMAKE_SYSTEM_NAME=Linux \
+-DCMAKE_SYSTEM_PROCESSOR=k1om \
+-DCMAKE_FIND_ROOT_PATH=$MPSSDIR \
+-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER \
+-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+-DCMAKE_INSTALL_PREFIX=$SRC/install-mic \
+$SRC/
+cd $BUILDTREE && make install VERBOSE=1
+
+mkdir -p $BUILDTREE/lib/pkgconfig
+cp $SRC/freetype2.pc $BUILDTREE/lib/pkgconfig/
+
+fi
+
+if test ${XEONX64BUILD+defined}; then
+
+# XEON x64 Build #
+BUILDTREE=$SRC/build
+
+# Clean Build Tree #
+if test ${INCREMENTDISABLE+defined}; then
+echo "Incremental Build disabled"
+rm -rf $BUILDTREE
+else
+echo "Incremental Build enabled"
+fi
+mkdir -p $BUILDTREE
+
+# Build x64 #
+
+cd $BUILDTREE \
+&& CC=icc CXX=icpc \
+CXXFLAGS="-fPIC" \
+cmake3 \
+-DCMAKE_INSTALL_PREFIX=$SRC/install-x64 \
+$SRC/
+cd $BUILDTREE && make install VERBOSE=1
+
+fi
+
